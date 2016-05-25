5291783a2e599199d6000b23d8bdd21fa8d8508b
Yumi Kim <yumikimsk@gmail.com>
2016-05-25 15:26:30 -0400

    add links to social media sites

--- build/footer.php
+++ build/footer.php
@@ -7,10 +7,10 @@
             yumikimsk@gmail.com<br>
             1.302.442.3125
             <p>
-                <a href="" target="_blank"><i class="icon fa fa-github-square fa-lg"></i></a>
-                <a href="" target="_blank"><i class="icon fa fa-linkedin-square fa-lg"></i></a>
-                <a href="" target="_blank"><i class="icon fa fa-twitter-square fa-lg"></i></a>
-                <a href="" target="_blank"><i class="icon fa fa-tumblr-square fa-lg"></i></a>
+                <a href="https://github.com/YouMeKim/" target="_blank"><i class="icon fa fa-github-square fa-lg"></i></a>
+                <a href="https://www.linkedin.com/in/yumi-kim-39110047" target="_blank"><i class="icon fa fa-linkedin-square fa-lg"></i></a>
+                <a href="https://twitter.com/YumiKimRIT" target="_blank"><i class="icon fa fa-twitter-square fa-lg"></i></a>
+                <a href="http://yumikimrit.tumblr.com/" target="_blank"><i class="icon fa fa-tumblr-square fa-lg"></i></a>
             </p>
         </div>
     </div>
